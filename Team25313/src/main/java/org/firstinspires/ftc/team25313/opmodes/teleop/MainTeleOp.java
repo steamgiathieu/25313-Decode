39ea7ec606cfdeda40ca7855bef8df237d40d73f
minhhdinh192 <minhh.notminh@gmail.com>
2025-11-05 21:56:08 +0700

    Update import in TeleOp

--- Team25313/src/main/java/org/firstinspires/ftc/team25313/opmodes/teleop/MainTeleOp.java
+++ Team25313/src/main/java/org/firstinspires/ftc/team25313/opmodes/teleop/MainTeleOp.java
@@ -2,13 +2,13 @@ package org.firstinspires.ftc.team25313.opmodes.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import org.firstinspires.ftc.team25313.subsystems.drivetrain;
-//import org.firstinspires.ftc.team25313.subsystems.intake;
-//import org.firstinspires.ftc.team25313.subsystems.outtake;
-//import org.firstinspires.ftc.team25313.subsystems.lift;
-//import org.firstinspires.ftc.team25313.subsystems.pathing;
-//import org.firstinspires.ftc.team25313.subsystems.sensor;
-//import org.firstinspires.ftc.team25313.subsystems.vision;
+import org.firstinspires.ftc.team25313.subsystems.drivetrain.Drivetrain;
+import org.firstinspires.ftc.team25313.subsystems.intake.ArtifactCollector;
+import org.firstinspires.ftc.team25313.subsystems.outtake.ArtifactLauncher;
+import org.firstinspires.ftc.team25313.subsystems.lift.Lift;
+import org.firstinspires.ftc.team25313.subsystems.vision.AutoAimAssist;
+import org.firstinspires.ftc.team25313.subsystems.vision.Camera;
+import org.firstinspires.ftc.team25313.subsystems.vision.DetectArtifactProcessor;
 
 @TeleOp(name = "Main TeleOp", group = "Manual")
 public class MainTeleOp extends LinearOpMode {
