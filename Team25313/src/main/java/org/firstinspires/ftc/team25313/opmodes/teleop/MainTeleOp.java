37edcad79c370a57fc9ff98a87b3e242c86ca51b
minhhdinh192 <minhh.notminh@gmail.com>
2025-12-09 21:41:26 +0700

    i want to test vision...

--- Team25313/src/main/java/org/firstinspires/ftc/team25313/opmodes/teleop/MainTeleOp.java
+++ Team25313/src/main/java/org/firstinspires/ftc/team25313/opmodes/teleop/MainTeleOp.java
@@ -2,22 +2,51 @@ package org.firstinspires.ftc.team25313.opmodes.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.acmerobotics.dashboard.FtcDashboard;
 
 import org.firstinspires.ftc.team25313.subsystems.drivetrain.DriveSubsystem;
 import org.firstinspires.ftc.team25313.subsystems.intake.ArtifactCollector;
+import org.firstinspires.ftc.team25313.subsystems.vision.GoalPose;
+import org.firstinspires.ftc.team25313.subsystems.vision.ObeliskPattern;
+import org.firstinspires.ftc.team25313.subsystems.vision.VisionSubsystem;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 @TeleOp(name = "Main TeleOp", group = "TeleOp")
 public class MainTeleOp extends LinearOpMode {
     private DriveSubsystem driveSubsystem;
     private ArtifactCollector intake;
+    private VisionSubsystem vision;
 
     @Override
     public void runOpMode() {
         driveSubsystem = new DriveSubsystem(hardwareMap);
         intake = new ArtifactCollector(hardwareMap);
+        vision = new VisionSubsystem(hardwareMap);
 
         while (opModeIsActive()) {
+            vision.update();
+
+            telemetry.addLine("=== Data of Vision ===");
+            if (vision.hasTag()) {
+                AprilTagDetection det = vision.getCurrentTag();
+                telemetry.addData("TagID", det.id);
+            }
+
+            // Tag Goal pose
+            GoalPose gp = vision.getGoalPose();
+            if (gp != null) {
+                telemetry.addData("tx", gp.tx);
+                telemetry.addData("ty", gp.ty);
+                telemetry.addData("tz", gp.tz);
+                telemetry.addData("distance cm", gp.distanceCm());
+                telemetry.addData("yaw", gp.yawDeg);
+            }
+
+            // Obelisk motif
+            ObeliskPattern.Color[] motif = vision.getObeliskPattern();
+            if (motif != null) {
+                telemetry.addData("Motif", motif[0] + " - " + motif[1] + " - " + motif[2]);
+            }
+
             double verticalRate = 0.5;
             double horizontalRate = 0.5;
             double rotateRate = 0.4;
@@ -31,7 +60,7 @@ public class MainTeleOp extends LinearOpMode {
             else intake.stop();
 
             // Regular telemetry (Driver Station) and Dashboard telemetry
-            telemetry.addLine("=== Telemetry ===");
+            telemetry.addLine("=== Data of Drivetrain ===");
             telemetry.addData("Y", forward);
             telemetry.addData("X", strafe);
             telemetry.addData("Z", rotate);
--- Team25313/src/main/java/org/firstinspires/ftc/team25313/subsystems/vision/VisionSubsystem.java
+++ Team25313/src/main/java/org/firstinspires/ftc/team25313/subsystems/vision/VisionSubsystem.java
@@ -1,25 +1,81 @@
 package org.firstinspires.ftc.team25313.subsystems.vision;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import android.util.Size;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import org.firstinspires.ftc.team25313.Constants;
 import org.firstinspires.ftc.vision.VisionPortal;
+
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+import java.util.List;
+
 public class VisionSubsystem {
-    private final VisionPortal portal;
-    private final AprilTagProcessor tag;
+
+    public VisionPortal portal;
+    public AprilTagProcessor tag;
+
+    // Lưu thông tin AprilTag hiện thời
+    private AprilTagDetection bestTag;
+    private GoalPose latestGoalPose;
+    private ObeliskPattern.Color[] latestPattern;
 
     public VisionSubsystem(HardwareMap hw) {
-        tag = new AprilTagProcessor.Builder().build();
+
+        tag = new AprilTagProcessor.Builder()
+                .setDrawTagID(true)
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .build();
 
         portal = new VisionPortal.Builder()
-                .setCamera(hw.get(WebcamName.class, "Webcam 1"))
+                .setCamera(hw.get(WebcamName.class, Constants.webcamName))
                 .addProcessor(tag)
+                .setCameraResolution(new Size(1280, 720))
                 .enableLiveView(true)
                 .build();
     }
 
-    public VisionPortal getPortal() {
-        return portal;
+    // Cập nhật khung hình
+    public void update() {
+        List<AprilTagDetection> dets = tag.getDetections();
+        if (dets == null || dets.isEmpty()) {
+            bestTag = null;
+            latestGoalPose = null;
+            latestPattern = null;
+            return;
+        }
+
+        bestTag = dets.get(0);   // lấy tag gần nhất đã qua filter của SDK
+
+        int id = bestTag.id;
+
+        // ---- Nếu là tag goal (ID 24 đỏ, 20 xanh) ----
+        if (id == 24 || id == 20) {
+            double tx = bestTag.ftcPose.x;
+            double ty = bestTag.ftcPose.y;
+            double tz = bestTag.ftcPose.z;
+            double yaw = bestTag.ftcPose.yaw;
+
+            latestGoalPose = new GoalPose(tx, ty, tz, yaw);
+        }
+
+        // ---- Nếu là obelisk motif ----
+        else if (id == 21 || id == 22 || id == 23) {
+            latestPattern = ObeliskPattern.decode(id);
+        }
     }
+
+    // GETTERS
+
+    public boolean hasTag() { return bestTag != null; }
+
+    public AprilTagDetection getCurrentTag() { return bestTag; }
+
+    public GoalPose getGoalPose() { return latestGoalPose; }
+
+    public ObeliskPattern.Color[] getObeliskPattern() { return latestPattern; }
 }
