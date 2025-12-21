4f60ce33f82df2d3b069dfa89bbf3242f62e2518
GetInHod <vminhhoang15012010@gmail.com>
2025-12-21 15:46:48 +0700

    merging #2

--- Team25313/src/main/java/org/firstinspires/ftc/team25313/opmodes/teleop/MainTeleOp.java
+++ Team25313/src/main/java/org/firstinspires/ftc/team25313/opmodes/teleop/MainTeleOp.java
@@ -3,90 +3,106 @@ package org.firstinspires.ftc.team25313.opmodes.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptAprilTagMultiPortal;
+import org.firstinspires.ftc.team25313.Constants;
+import org.firstinspires.ftc.team25313.FTCObject;
+import org.firstinspires.ftc.team25313.Utility;
 import org.firstinspires.ftc.team25313.subsystems.drivetrain.DriveSubsystem;
+import org.firstinspires.ftc.team25313.subsystems.drivetrain.pid.HeadingPID;
 import org.firstinspires.ftc.team25313.subsystems.intake.ArtifactCollector;
 import org.firstinspires.ftc.team25313.subsystems.outtake.ArtifactLauncher;
-import org.firstinspires.ftc.team25313.subsystems.sensor.REVDistanceSensor;
+import org.firstinspires.ftc.team25313.subsystems.vision.GoalPose;
+import org.firstinspires.ftc.team25313.subsystems.vision.ObeliskPattern;
+import org.firstinspires.ftc.team25313.subsystems.vision.VisionSubsystem;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.slf4j.helpers.Util;
 
 @TeleOp(name = "Main TeleOp", group = "TeleOp")
 public class MainTeleOp extends LinearOpMode {
-    // Subsystems
     private DriveSubsystem driveSubsystem;
-//    private ArtifactCollector intake;
-//    private ArtifactLauncher outtake;
-//    private REVDistanceSensor sensorDistance;
-
-    // button edge detection
-
-    private boolean UpPrev = false;
-    private boolean DownPrev = false;
+    private ArtifactCollector intake;
+    private ArtifactLauncher outtake;
+//    private VisionSubsystem vision;
 
     @Override
     public void runOpMode() {
         driveSubsystem = new DriveSubsystem(hardwareMap);
-//        intake = new ArtifactCollector(hardwareMap);
-//        outtake = new ArtifactLauncher(hardwareMap);
-//        sensorDistance = new REVDistanceSensor(hardwareMap);
+        intake = new ArtifactCollector(hardwareMap);
+        outtake = new ArtifactLauncher(hardwareMap);
 
-        telemetry.addLine("Initialized Main TeleOp!");
+        intake.stop();
+        outtake.setLauncherOff();
+//        vision = new VisionSubsystem(hardwareMap);
+//        boolean isLockToGoal = false;
+//        HeadingPID headingPID = new HeadingPID(0.02, 0, 0.02);
+
         waitForStart();
+        if (isStopRequested()) return;
 
-        // =============================================
-        // DRIVETRAIN CONTROL (Robot-Centric)
-        // =============================================
         while (opModeIsActive()) {
-            double forward = gamepad1.left_stick_y;
-            double strafe = -gamepad1.left_stick_x;
-            double rotate = -gamepad1.right_stick_x;
+//            vision.update();
 
-            driveSubsystem.drive(forward, strafe, rotate);
-
-
-            // =============================================
-            // INTAKE CONTROL
-            // =============================================
-//            if (gamepad1.y) intake.collect();
-//            else if (gamepad1.a) intake.reverse();
-//            else intake.stop();
-
-            // =============================================
-            // LAUNCHER CONTROL
-            // =============================================
-
-//            if (gamepad1.x) outtake.startShooter();
-//            else if (gamepad1.b) outtake.stopShooter();
-//
-//            boolean Up = gamepad1.dpad_up;
-//            boolean Down = gamepad1.dpad_down;
-//            if (Up && !UpPrev) {
-//                outtake.increasePowerLevel();
+//            if (gamepad1.a) {
+//                isLockToGoal = true;
 //            }
-//            else if (Down && !DownPrev) {
-//                outtake.decreasePowerLevel();
+//            else if (gamepad1.b) {
+//                isLockToGoal = false;
 //            }
-//            UpPrev = Up;
-//            DownPrev = Down;
-//
-//            // Servo
-//            if (gamepad1.left_bumper) outtake.push();
-//            else outtake.rest();
-//
-//            // Sensor
-//            double Data = sensorDistance.GetData();
+            double verticalRate = 1;
+            double horizontalRate = 1;
+            double rotateRate = 1;
+            // Gamepad left_stick_y is typically negative when pushed forward.
+            // Invert the Y axis so a forward push produces positive forward power.
+            double forward = gamepad1.left_stick_y * verticalRate;
+            double strafe = -gamepad1.left_stick_x * horizontalRate;
+            double rotate = driveSubsystem.fixedRotate(-gamepad1.right_stick_x, rotateRate);
 
-            // =============================================
-            // TELEMETRY
-            // =============================================
-            telemetry.addLine("=== Posterboy's Telemetry ===");
-            telemetry.addData("Y", forward);
-            telemetry.addData("X", strafe);
-            telemetry.addData("Z", rotate);
-//            telemetry.addData("Shooter Power", outtake.getCurrentPowerLevel());
-//            telemetry.addData("Intake Mode",
-//                    gamepad1.right_bumper ? "COLLECT" :
-//                            gamepad1.left_bumper ? "REVERSE" : "STOP");
-//            telemetry.addData("Distance", "%.1f", Data);
-            telemetry.update();
+//            telemetry.addLine(Constants.botName);
+//            telemetry.addLine("=== Data of Vision ===");
+//            if (vision.hasTag()) {
+//                AprilTagDetection det = vision.getCurrentTag();
+//                telemetry.addData("TagID", det.id);
+//            }
+
+            // Tag Goal pose
+//            GoalPose gp = vision.getGoalPose();
+//            if (gp != null) {
+//                telemetry.addData("tx", gp.tx);
+//                telemetry.addData("ty", gp.ty);
+//                telemetry.addData("tz", gp.tz);
+//                telemetry.addData("distance cm", gp.distanceCm());
+//                telemetry.addData("yaw", gp.yawDeg);
+//                if (isLockToGoal) {
+//                    double yawError = gp.yawDeg;
+//                    rotate = headingPID.calculate(0, yawError);
+//                    rotate = Math.max(-1, Math.min(1, rotate));
+//                }
+//            }
+
+            // Obelisk motif
+//            ObeliskPattern.Color[] motif = vision.getObeliskPattern();
+//            if (motif != null) {
+//                telemetry.addData("Motif", motif[0] + " - " + motif[1] + " - " + motif[2]);
+//            }
+
+            driveSubsystem.driveRobotRelated(forward, strafe, rotate);
+
+            if (gamepad2.y) intake.collect();
+            else if (gamepad2.a) intake.stop();
+
+            if (gamepad2.x) outtake.setLauncherReady();
+            else if (gamepad2.dpad_up) outtake.setBaseLaunch();
+            else if (gamepad2.dpad_down) outtake.setGoalLaunch();
+            else if (gamepad2.b) outtake.setLauncherOff();
+
+            outtake.update();
+
+            // Regular telemetry (Driver Station) and Dashboard telemetry
+            telemetry.addLine("accelerada");
+            Utility.teleDrivePose(telemetry, forward, strafe, rotate);
+            Utility.teleIntake(telemetry, intake.isRunning());
+            Utility.teleOuttake(telemetry, outtake.isRunning(), outtake.getPower());
+            Utility.teleUpdate(telemetry);
         }
     }
 }
