4d458f30a15c0346991485f74350cf4191eafbb0
tunglx <lexuantung19999@gmail.com>
2026-01-31 00:55:10 +0700

    change power intake due to change gear ratio

--- Team25313/src/main/java/org/firstinspires/ftc/team25313/Constants.java
+++ Team25313/src/main/java/org/firstinspires/ftc/team25313/Constants.java
@@ -21,9 +21,9 @@ public final class Constants {
     public static final String leftCollector = "leftCollector";
     public static final String rightCollector = "rightCollector";
     public static final double intakeMotorIn = 0.4;
-    public static double intakeMotorShoot = 0.3;
-    public static final double farZoneIntakeMotorShoot = 0.2;
-    public static final double closeZoneIntakeMotorShoot = 0.3;
+    public static double intakeMotorShoot = 0.32;
+    public static final double farZoneIntakeMotorShoot = 0.25;
+    public static final double closeZoneIntakeMotorShoot = 0.32;
     public static final double autoIntakeMotorShoot = 0.5;
     public static final double intakeServoIn = 0.8;
     public static final double shootingTime = 2000; //ms
